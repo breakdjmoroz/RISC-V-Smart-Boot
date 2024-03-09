d5c7360a1fd3d1fa2c6795caae89691655ada502
bdjm <danmoroz55@gmail.com>
2024-03-09 12:11:53 +0300

    Созадть функцию test() в main.c для проверки нового функционала

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include "../hdr/rvio.h"
 
+void test()
+{
+  rvprints("Hello, %s\n", "World!");
+  unsigned int a = 123456789u;
+  rvprintu("Number %d\n", a);
+}
+
 int main()
 {
-  rvprintf("Hello, %s\n", "World!");
-  unsigned int a = 123456789u;
-  rvprintf("Number %d\n", &a);
+  test();
   return 0;
 }
