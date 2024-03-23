7245683dbb0db79556418e402e7d30f41ebdc638
Phantom554 <150785804+Phantom554@users.noreply.github.com>
2024-03-23 07:36:39 -0400

    Create rvmath.h

---
+++ hdr/rvmath.h
@@ -0,0 +1,11 @@
+#ifndef RVMATH
+#define RVMATH
+
+#define DEC_BASE 10u
+#define BIN_BASE 2u
+#define HEX_BASE 16u
+
+unsigned int i_mod(const unsigned int a, const unsigned int b);
+unsigned int i_div(const unsigned int a, const unsigned int b);
+
+#endif
