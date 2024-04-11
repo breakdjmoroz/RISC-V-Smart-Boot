dcba8d0f6c2b8f8de660d495f12f319fa1f46dc8
bdjm <danmoroz55@gmail.com>
2024-04-11 13:05:21 +0300

    Оптимизировать деление на цело и взятие остатка от деления

--- hdr/rvmath.h
+++ hdr/rvmath.h
@@ -1,6 +1,10 @@
 #ifndef RVMATH
 #define RVMATH
 
+#define DEC_BASE 10u
+#define BIN_BASE 2u
+#define HEX_BASE 16u
+
 unsigned long long i_mod(const unsigned long long a, const unsigned long long b);
 unsigned long long i_div(const unsigned long long a, const unsigned long long b);
 
