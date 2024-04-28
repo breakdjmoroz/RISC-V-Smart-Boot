4f0bee3d4863e405ebc9006ae64209857f4dbe3e
bdjm <danmoroz55@gmail.com>
2024-04-28 15:24:19 +0300

    Добавить макро-функцию записи в регистр

--- hdr/register.h
+++ hdr/register.h
@@ -3,11 +3,35 @@
 
 #define MIP "mip"
 #define MIE "mie"
+#define MISA "misa"
+#define MTVAL "mtval"
+#define MTVEC "mtvec"
 #define MCAUSE "mcause"
+#define MIMPID "mimpid"
+#define MARCHID "marchid"
+#define MHARTID "mhartid"
 #define MSTATUS "mstatus"
+#define MSCRATCH "mscratch"
+#define MVENDORID "mvendorid"
 
 
-#define csr_read(csr)({ register unsigned long __v; __asm__ __volatile__("csrr %0, " csr : "=r"(__v):: "memory");__v;})
+
+#define csr_read(csr)({ register unsigned long v; __asm volatile("csrr %0, " csr : "=r"(v):: "memory");__v;})
+
+#define csr_write(csr, val) \
+    ({unsigned long __v = (unsigned long)(val);__asm volatile("csrw " csr ", %0":: "rK"(v): "memory");})
+
+/*dont work normal*/
+/*#define csr_modify(csr, set_bit, bitpos)                                                            \
+    ({                                                                                              \
+        unsigned long __mask = (1UL << (bitpos));                                                   \
+        unsigned long __read_val;                                                                   \
+        if (set_bit) {                                                                              \
+            __asm volatile("csrs " csr ", %0":: "r"(mask): "memory");   \
+        } else {                                                                                    \
+            __asm volatile("csrc " csr ", %0":: "r"(__mask): "memory");    \
+        }                                                                                                \           
+    })*/
 
 
 #endif
